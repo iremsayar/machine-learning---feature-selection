a4be1c3bd6590daf0ac4b939ecc00c956adf5914
iremsayar <39015459+iremsayar@users.noreply.github.com>
2021-02-08 23:42:56 +0300

    Add files via upload

---
+++ color.m
@@ -0,0 +1,24 @@
+
+srcFiles = dir('C:\Users\Ýrem SAYAR\Desktop\ml\features\lichenplanus\*.jpg');  % the folder in which ur images exists
+
+for i = 1 : length(srcFiles)
+    
+    filename = strcat('C:\Users\Ýrem SAYAR\Desktop\ml\features\lichenplanus\',srcFiles(i).name);
+ 
+    I = imread(filename);
+    [w,h]=size(I);
+   I = 255-I;
+    R = I(:,:,1);
+    G = I(:,:,2);
+    B = I(:,:,3);
+
+R_av = (sum(sum(R))) / (w*h);
+G_av = (sum(sum(G))) / (w*h);
+B_av = (sum(sum(B))) / (w*h);
+top = R_av + G_av + B_av;
+mean = top/3;
+    x = [R_av G_av B_av top mean];
+
+    featureslichen(i,:) = x;
+    
+end
